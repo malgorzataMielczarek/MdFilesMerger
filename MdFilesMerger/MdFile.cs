6d637e2cdfdf5809ce8970f6b5de7b131b40b1f7
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-04-26 15:55:47 +0200

    Rename FileCustomInfo to MdFile

---
+++ MdFilesMerger/MdFile.cs
@@ -0,0 +1,34 @@
+﻿namespace MdFilesMerger
+{
+    internal class MdFile
+    {
+        public FileInfo FileInfo { get; private set; }
+        public string[] SubDirectories { get; private set; }
+        public MdFile(FileInfo fileInfo, string mainDirectory)
+        {
+            FileInfo = fileInfo;
+            SubDirectories = GetSubDirectories(mainDirectory);
+        }
+
+        private string[] GetSubDirectories(string mainDirectory)
+        {
+            if(FileInfo.DirectoryName != null)
+            {
+                string dirs = FileInfo.DirectoryName.Replace(mainDirectory, "");
+                var directories = dirs.Split('\\', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+                return directories;
+            }
+            return Array.Empty<string>();
+        }
+        //TODO: Open the file and read first not empty line of text from it. If it is a header (starts with '#') return it. Else return file name, without extension.
+        public string GetFileHeader()
+        {
+            return GetFileHeader(FileInfo);
+        }
+        public static string GetFileHeader(FileInfo file)
+        {
+            return file.Name.Replace(file.Extension, "");
+        }
+    }
+}
