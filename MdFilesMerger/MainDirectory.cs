ed7dfa64c68d0a46d57bf0864d2d8e65a387934c
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-05-08 15:41:38 +0200

    Add MainDirectory class

---
+++ MdFilesMerger/MainDirectory.cs
@@ -0,0 +1,45 @@
+﻿namespace MdFilesMerger
+{
+    internal class MainDirectory
+    {
+        public string Path => _path;
+
+        public MainDirectory()
+        {
+            _path = String.Empty;
+
+            SetMainDirectory(Program.MAIN_DIRECTORY_PATH);
+        }
+
+        public bool Display()
+        {
+            if(String.IsNullOrEmpty(_path))
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Domyślna ścieżka do katalogu: {0}", _path);
+                return true;
+            }
+        }
+
+        public bool SetMainDirectory(string path)
+        {
+            if(Directory.Exists(path))
+            {
+                var directory = new DirectoryInfo(path);
+                _path = directory.FullName;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        private string _path;
+    }
+}
