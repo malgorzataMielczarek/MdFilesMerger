18d0c32a2547950a0a3cf92a4bf307efeff18b04
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-05-22 14:04:38 +0200

    Create base services

---
+++ MdFilesMerger.App/Abstract/IService.cs
@@ -0,0 +1,13 @@
+﻿using MdFilesMerger.Domain.Abstract;
+
+namespace MdFilesMerger.App.Abstract
+{
+    public interface IService<T> where T : IItem
+    {
+        public int AddItem(T item);
+        public T? GetItemById(int id);
+        public IReadOnlyList<T> GetItemsByName(string name);
+        public IReadOnlyList<T> GetAllItems();
+        public int GetNewId();
+    }
+}
---
+++ MdFilesMerger.App/Common/BaseService.cs
@@ -0,0 +1,79 @@
+﻿using MdFilesMerger.App.Abstract;
+using MdFilesMerger.Domain.Common;
+
+namespace MdFilesMerger.App.Common
+{
+    public class BaseService<T> : IService<T> where T : BaseItem
+    {
+        protected List<T> _items;
+
+        public BaseService()
+        {
+            _items = new List<T>();
+        }
+
+        public BaseService(List<T> items)
+        {
+            _items = items;
+        }
+
+        public virtual int AddItem(T item)
+        {
+            if (item.Id == 0 || GetItemById(item.Id) != null)
+            {
+                item.Id = GetNewId();
+            }
+
+            _items.Add(item);
+
+            return item.Id;
+        }
+
+        public T? GetItemById(int id)
+        {
+            foreach (var item in _items)
+            {
+                if (item.Id == id)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+        public IReadOnlyList<T> GetItemsByName(string name)
+        {
+            List<T> list = new List<T>();
+            foreach (var item in _items)
+            {
+                if (item.Name == name)
+                    list.Add(item);
+            }
+
+            return list;
+        }
+
+        public IReadOnlyList<T> GetAllItems()
+        {
+            return _items;
+        }
+
+        public virtual int GetNewId()
+        {
+            int newId = 0;
+
+            foreach (var item in _items)
+            {
+                if (item.Id > newId)
+                {
+                    newId = item.Id;
+                }
+            }
+
+            newId++;
+
+            return newId;
+        }
+    }
+}
