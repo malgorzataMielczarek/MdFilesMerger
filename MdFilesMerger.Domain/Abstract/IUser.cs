67b8ab50ea26cb461322460254ce15611f890622
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-05-26 12:44:09 +0200

    User interface with dascription

--- MdFilesMerger.Domain/Abstract/IUser.cs
+++ MdFilesMerger.Domain/Abstract/IUser.cs
@@ -1,7 +1,14 @@
 ﻿namespace MdFilesMerger.Domain.Abstract
 {
+    /// <summary>
+    /// User interface
+    /// </summary>
     public interface IUser : IItem
     {
+        /// <value>Encrypted password used to authorize user</value>
         public string? Password { get; set; }
+
+        ///<value>Value used for password encryptation/decryptation</value>
+        public string Salt { get; }
     }
 }
