cc2b55758f2f4025d893a953019298d5655372f4
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-05-26 12:34:58 +0200

    Describe base model interface

--- MdFilesMerger.Domain/Abstract/IItem.cs
+++ MdFilesMerger.Domain/Abstract/IItem.cs
@@ -1,8 +1,14 @@
 ﻿namespace MdFilesMerger.Domain.Abstract
 {
+    /// <summary>
+    /// Base iterface for all models.
+    /// </summary>
     public interface IItem
     {
+        /// <value>Distinct identification number of element</value>
         public int Id { get; set; }
+
+        /// <value>Description of element. It will be user name or path of directory/file</value>
         public string? Name { get; set; }
     }
 }
