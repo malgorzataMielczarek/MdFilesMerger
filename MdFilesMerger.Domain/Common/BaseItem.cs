78486dfb9527afcaba64d7b02845095e3b8089d0
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-05-26 12:36:42 +0200

    Describe and implement base model interface

--- MdFilesMerger.Domain/Common/BaseItem.cs
+++ MdFilesMerger.Domain/Common/BaseItem.cs
@@ -2,23 +2,51 @@
 
 namespace MdFilesMerger.Domain.Common
 {
+    /// <summary>
+    /// Implementation of base model for all models
+    /// </summary>
     public class BaseItem : IItem
     {
         public int Id { get; set; }
         public string? Name { get; set; }
 
+        /// <summary>
+        /// Sets <see cref="Id">Id</see> to <see langword="0"/> and <see cref="Name">Name</see> to <see langword="null"/>.
+        /// </summary>
         public BaseItem()
         {
-
+            Id = 0;
+            Name = null;
         }
 
+        /// <summary>
+        /// Sets <see cref="Id">Id</see> to <paramref name="id"/> and <see cref="Name">Name</see> to <see langword="null"/>.
+        /// </summary>
+        /// <param name="id">Value of <see cref="Id">Id</see>.</param>
         public BaseItem(int id)
         {
             Id = id;
+            Name = null;
         }
 
-        public BaseItem(int id, string? name) : this(id)
+        /// <summary>
+        /// Sets <see cref="Id">Id</see> to <see langword="0"/> and <see cref="Name">Name</see> to <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Value of <see cref="Name">Name</see>.</param>
+        public BaseItem(string? name)
         {
+            Id = 0;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Sets <see cref="Id">Id</see> to <paramref name="id"/> and <see cref="Name">Name</see> to <paramref name="name"/>.
+        /// </summary>
+        /// <param name="id">Value of <see cref="Id">Id</see>.</param>
+        /// <param name="name">Value of <see cref="Name">Name</see>.</param>
+        public BaseItem(int id, string? name)
+        {
+            Id = id;
             Name = name;
         }
     }
