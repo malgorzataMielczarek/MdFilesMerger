4316ab77a53ac04226e58c35d5620b8ce912c438
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-06-01 17:37:14 +0200

    Modify IService to the most basic service interface, for all services.

--- MdFilesMerger.App/Abstract/IService.cs
+++ MdFilesMerger.App/Abstract/IService.cs
@@ -2,13 +2,23 @@
 
 namespace MdFilesMerger.App.Abstract
 {
+    /// <summary>
+    ///     Base interface for all services in <see cref="App"/> project.
+    ///     <para>
+    ///         <b> Inheritance: </b> IService&lt;T&gt; <br/><b> Implementations: </b><see cref="Concrete.MenuActionService"/>
+    ///     </para>
+    /// </summary>
+    /// <typeparam name="T"> Type of stored and serviced objects. </typeparam>
+    /// <seealso cref="Concrete.MenuActionService"> MdFilesMerger.App.Concrete.MenuActionService </seealso>
     public interface IService<T> where T : IItem
     {
-        public int AddItem(T item);
-        public int AddRange(List<T> items);
-        public T? GetItemById(int id);
-        public IReadOnlyList<T> GetItemsByName(string name);
-        public IReadOnlyList<T> GetAllItems();
-        public int GetNewId();
+        /// <summary>
+        ///     Gets stored <see cref="IItem"/> object by it's identification number.
+        /// </summary>
+        /// <param name="id"> Value of <see cref="IItem.Id"/> of searched object. </param>
+        /// <returns>
+        ///     Found <see cref="IItem"/> object, or <see langword="null"/>, if no object was found.
+        /// </returns>
+        public T? GetById(int id);
     }
 }
