3d905a5bc6d3dae0c32f1b46d21e486c9b5d0120
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-06-21 15:08:39 +0200

    IgnoredFileService tests update

--- MdFilesMerger.Tests/IgnoredFileServiceTests.cs
+++ MdFilesMerger.Tests/IgnoredFileServiceTests.cs
@@ -11,16 +11,24 @@ namespace MdFilesMerger.Tests
     public class IgnoredFileServiceTests
     {
         [Fact]
-        public void IgnoredToSelectedFileConversion()
+        public void CanConverseWholeList()
         {
             // Arrange
-            DateTime now = DateTime.Now;
-            IIgnoredFile file = new IgnoredFile(1, "README.md", 1, now);
-            List<IIgnoredFile> files = new List<IIgnoredFile>();
-            files.Add(file);
-            MainDirectory mainDirectory = new MainDirectory(1, "C:/Users/mielczarek/source/repos/KursZostanProgramistaASPdotNET", 1);
+            string mainDirPath = "C:\\Users\\mielczarek\\source\\repos\\MdFilesMerger"; // "ExampleDir";
+            MainDirectory mainDirectory = new MainDirectory(1, mainDirPath, 1, DateTime.Now);
+            SelectedFile selectedFile = new SelectedFile(1, "Example.md", 1, DateTime.Now, "Title");
+
             var mock = new Mock<IMainDirectoryService>();
             mock.Setup(s => s.ReadById(1)).Returns(mainDirectory);
+
+            var ignoredMock = new Mock<IIgnoredFile>();
+            ignoredMock.Setup(i => i.ToSelectedFile(mainDirPath)).Returns(selectedFile);
+            ignoredMock.Setup(i => i.MainDirId).Returns(mainDirectory.Id);
+            var file1 = ignoredMock.Object;
+            List<IIgnoredFile> files = new List<IIgnoredFile>();
+            files.Add(ignoredMock.Object);
+            files.Add(ignoredMock.Object);
+            files.Add(ignoredMock.Object);
 
             // Act
             var result = IgnoredFileService.ToSelectedFile(files, mock.Object);
@@ -30,11 +38,8 @@ namespace MdFilesMerger.Tests
             result.Should().AllBeOfType<SelectedFile>();
             result.Should().NotBeNullOrEmpty(); // Assert.NotNull(result); Assert.NotEmpty(result);
             result.Should().NotContainNulls();
-            result.Should().HaveCount(1);   // Assert.Single(result);
-            result[0].Name.Should().BeEquivalentTo("README.md");    // Assert.Equal("README.md", result[0].Name);
-            result[0].MainDirId.Should().Be(1); // Assert.Equal(1, result[0].MainDirId);
-            result[0].Title.Should().BeEquivalentTo("Kurs \"Zostañ programist¹ ASP.NET\" - notatki");   // Assert.Equal("Kurs \"Zostañ programist¹ ASP.NET\" - notatki", result[0].Title);
-            result[0].ModifiedDate.Should().BeAfter(now).And.BeBefore(DateTime.Now);    // Assert.True(result[0].ModifiedDate > now && result[0].ModifiedDate < DateTime.Now);
+            result.Should().HaveSameCount(files);
+            result.Should().OnlyContain(item => item == selectedFile);
         }
     }
 }
