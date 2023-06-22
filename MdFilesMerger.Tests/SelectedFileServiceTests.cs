505af8921b3d0d182d14fe9c5b8bfc61b544a577
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-06-22 16:07:56 +0200

    Add tests for SelectedFileService

---
+++ MdFilesMerger.Tests/SelectedFileServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using MdFilesMerger.App.Concrete;
+using MdFilesMerger.Domain.Entity;
+using Xunit;
+using FluentAssertions;
+
+namespace MdFilesMerger.Tests
+{
+    public class SelectedFileServiceTests
+    {
+        [Fact]
+        public void CanUpdateTitle()
+        {
+            // Arrange
+            int id = 100000;
+            var date = DateTime.Now.AddDays(-7);
+            string oldTitle = "Old title", newTitle = "New title";
+            SelectedFile selectedFile = new SelectedFile() { Id = id, ModifiedDate = date, Title = oldTitle };
+
+            SelectedFileService selectedFileService = new SelectedFileService(new MainDirectoryService());
+            selectedFileService.Create(selectedFile);
+
+            // Act
+            var result = selectedFileService.UpdateTitle(selectedFile.Id, newTitle);
+
+            // Assert
+            result.Should().Be(selectedFile.Id);
+            selectedFile.ModifiedDate.Should().BeAfter(date).And.BeBefore(DateTime.Now);
+            selectedFile.Title.Should().Be(newTitle);
+        }
+    }
+}
