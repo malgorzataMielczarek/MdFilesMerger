df23ab1ca2446cf79dabf377cfce61fce44deb25
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-06-26 16:16:11 +0200

    BaseDirectoryServiceTests rename tests (MethodName_Scenario_ExpectedBehavior)

--- MdFilesMerger.Tests/App/Common/BaseDirectoryServiceTests.cs
+++ MdFilesMerger.Tests/App/Common/BaseDirectoryServiceTests.cs
@@ -9,31 +9,7 @@ namespace MdFilesMerger.Tests.App.Common
     public class BaseDirectoryServiceTests
     {
         [Fact]
-        public void CanUpdatePath()
-        {
-            // Arrange
-            int id = 100000;
-            string oldPath = "OldPath", validPath = "ValidPath";
-            var dir = new Mock<IDirectory>();
-            dir.SetupGet(f => f.Id).Returns(id);
-            dir.SetupGet(f => f.Name).Returns(oldPath);
-            dir.SetupSet(f => f.Name = It.IsAny<string>());
-            dir.Setup(f => f.SetPath(validPath)).Returns(true);
-
-            var baseDirectoryService = new BaseDirectoryService<IDirectory>();
-            baseDirectoryService.Create(dir.Object);
-
-            // Act
-            var result = baseDirectoryService.UpdatePath(id, validPath);
-
-            // Assert
-            result.Should().Be(id);
-            dir.Verify(f => f.SetPath(validPath), Times.Once());
-            dir.VerifySet(f => f.Name = It.IsAny<string>(), Times.Never());
-        }
-
-        [Fact]
-        public void PathNotChangeForInvalidId()
+        public void UpdatePath_InvalidId_NotTryToSetPath()
         {
             // Arrange
             int id = 100000;
@@ -57,7 +33,7 @@ namespace MdFilesMerger.Tests.App.Common
         }
 
         [Fact]
-        public void PathNotChangeForInvalidValue()
+        public void UpdatePath_SetInvalidPath_RestoreName()
         {
             // Arrange
             int id = 100000;
@@ -78,6 +54,30 @@ namespace MdFilesMerger.Tests.App.Common
             result.Should().Be(-1);
             dir.Verify(f => f.SetPath(invalidPath), Times.Once());
             dir.VerifySet(f => f.Name = It.IsAny<string>(), Times.Once());
+        }
+
+        [Fact]
+        public void UpdatePath_ValidPath_NotRestoreName()
+        {
+            // Arrange
+            int id = 100000;
+            string oldPath = "OldPath", validPath = "ValidPath";
+            var dir = new Mock<IDirectory>();
+            dir.SetupGet(f => f.Id).Returns(id);
+            dir.SetupGet(f => f.Name).Returns(oldPath);
+            dir.SetupSet(f => f.Name = It.IsAny<string>());
+            dir.Setup(f => f.SetPath(validPath)).Returns(true);
+
+            var baseDirectoryService = new BaseDirectoryService<IDirectory>();
+            baseDirectoryService.Create(dir.Object);
+
+            // Act
+            var result = baseDirectoryService.UpdatePath(id, validPath);
+
+            // Assert
+            result.Should().Be(id);
+            dir.Verify(f => f.SetPath(validPath), Times.Once());
+            dir.VerifySet(f => f.Name = It.IsAny<string>(), Times.Never());
         }
     }
 }
