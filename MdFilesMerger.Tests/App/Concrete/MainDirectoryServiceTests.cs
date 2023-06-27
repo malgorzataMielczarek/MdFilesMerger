28eefd713f0ca0ea4b97c4ac5f15e437a411ccad
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-06-27 10:52:13 +0200

    MainDirectoryServiceTests - adjust tests names

--- MdFilesMerger.Tests/App/Concrete/MainDirectoryServiceTests.cs
+++ MdFilesMerger.Tests/App/Concrete/MainDirectoryServiceTests.cs
@@ -9,44 +9,37 @@ namespace MdFilesMerger.Tests.App.Concrete
     public class MainDirectoryServiceTests
     {
         [Fact]
-        public void IsReturningItemsOnlyWithGivenMergedFileId()
+        public void FindAllFiles_InvalidId_ReturnsEmpty()
         {
             // Arrange
-            List<IMainDirectory> mainDirectories = new List<IMainDirectory>();
-            var dir1 = new MainDirectory(1, "Path1", 1, DateTime.Now);
-            var dir2 = new MainDirectory(2, "Path2", 2, DateTime.Now);
-            var dir3 = new MainDirectory(3, "Path3", 1, DateTime.Now);
-            var dir4 = new MainDirectory(4, "Path4", 1, DateTime.Now);
-            var dir5 = new MainDirectory(5, "Path5", 5, DateTime.Now);
-            var dir6 = new MainDirectory(6, "Path6", 1, DateTime.Now);
-            mainDirectories.Add(dir1);
-            mainDirectories.Add(dir2);
-            mainDirectories.Add(dir3);
-            mainDirectories.Add(dir4);
-            mainDirectories.Add(dir5);
-            mainDirectories.Add(dir6);
-
             MainDirectoryService mainDirectoryService = new MainDirectoryService();
-            mainDirectoryService.CreateRange(mainDirectories);
 
             // Act
-            var items = mainDirectoryService.ReadByMergedFileId(1);
+            var result = mainDirectoryService.FindAllFiles(-1);
 
             // Assert
-            items.Should().BeOfType<List<IMainDirectory>>();
-            items.Should().AllBeOfType<MainDirectory>();
-            items.Should().NotBeNullOrEmpty();
-            items.Should().HaveCountGreaterThanOrEqualTo(4);
-            items.Should().Contain(dir1);
-            items.Should().NotContain(dir2);
-            items.Should().Contain(dir3);
-            items.Should().Contain(dir4);
-            items.Should().NotContain(dir5);
-            items.Should().Contain(dir6);
+            result.Should().BeAssignableTo<IEnumerable<FileInfo>>();
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
         }
 
         [Fact]
-        public void IsReturningSorted()
+        public void FindAllFiles_Null_ReturnsEmpty()
+        {
+            // Arrange
+            MainDirectoryService mainDirectoryService = new MainDirectoryService();
+
+            // Act
+            var result = mainDirectoryService.FindAllFiles(null);
+
+            // Assert
+            result.Should().BeAssignableTo<IEnumerable<FileInfo>>();
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ReadByMergedFileId_UnsortedCollection_ReturnsSorted()
         {
             // Arrange
             List<IMainDirectory> mainDirectories = new List<IMainDirectory>();
@@ -81,33 +74,40 @@ namespace MdFilesMerger.Tests.App.Concrete
         }
 
         [Fact]
-        public void ReturningEmptyListForInvalidId()
+        public void ReadByMergedFileId_ValidMergedFileId_ReturnsOnlyItemsWithGivenMergedFileId()
         {
             // Arrange
+            List<IMainDirectory> mainDirectories = new List<IMainDirectory>();
+            var dir1 = new MainDirectory(1, "Path1", 1, DateTime.Now);
+            var dir2 = new MainDirectory(2, "Path2", 2, DateTime.Now);
+            var dir3 = new MainDirectory(3, "Path3", 1, DateTime.Now);
+            var dir4 = new MainDirectory(4, "Path4", 1, DateTime.Now);
+            var dir5 = new MainDirectory(5, "Path5", 5, DateTime.Now);
+            var dir6 = new MainDirectory(6, "Path6", 1, DateTime.Now);
+            mainDirectories.Add(dir1);
+            mainDirectories.Add(dir2);
+            mainDirectories.Add(dir3);
+            mainDirectories.Add(dir4);
+            mainDirectories.Add(dir5);
+            mainDirectories.Add(dir6);
+
             MainDirectoryService mainDirectoryService = new MainDirectoryService();
+            mainDirectoryService.CreateRange(mainDirectories);
 
             // Act
-            var result = mainDirectoryService.FindAllFiles(-1);
+            var items = mainDirectoryService.ReadByMergedFileId(1);
 
             // Assert
-            result.Should().BeAssignableTo<IEnumerable<FileInfo>>();
-            result.Should().NotBeNull();
-            result.Should().BeEmpty();
-        }
-
-        [Fact]
-        public void ReturningEmptyListForNull()
-        {
-            // Arrange
-            MainDirectoryService mainDirectoryService = new MainDirectoryService();
-
-            // Act
-            var result = mainDirectoryService.FindAllFiles(null);
-
-            // Assert
-            result.Should().BeAssignableTo<IEnumerable<FileInfo>>();
-            result.Should().NotBeNull();
-            result.Should().BeEmpty();
+            items.Should().BeOfType<List<IMainDirectory>>();
+            items.Should().AllBeOfType<MainDirectory>();
+            items.Should().NotBeNullOrEmpty();
+            items.Should().HaveCountGreaterThanOrEqualTo(4);
+            items.Should().Contain(dir1);
+            items.Should().NotContain(dir2);
+            items.Should().Contain(dir3);
+            items.Should().Contain(dir4);
+            items.Should().NotContain(dir5);
+            items.Should().Contain(dir6);
         }
     }
 }
