30e474977d24574716ae935730b8847402139e8e
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-06-27 14:36:09 +0200

    SelectedFileServiceTests - adjust test name, add InvalidId test case

--- MdFilesMerger.Tests/App/Concrete/SelectedFileServiceTests.cs
+++ MdFilesMerger.Tests/App/Concrete/SelectedFileServiceTests.cs
@@ -1,20 +1,39 @@
-﻿using MdFilesMerger.App.Concrete;
+﻿using FluentAssertions;
+using MdFilesMerger.App.Concrete;
 using MdFilesMerger.Domain.Entity;
 using Xunit;
-using FluentAssertions;
 
 namespace MdFilesMerger.Tests.App.Concrete
 {
     public class SelectedFileServiceTests
     {
         [Fact]
-        public void CanUpdateTitle()
+        public void UpdateTitle_InvalidId_ReturnsMinusOne()
         {
             // Arrange
-            int id = 100000;
-            var date = DateTime.Now.AddDays(-7);
+            var date = DateTime.Now;
             string oldTitle = "Old title", newTitle = "New title";
-            SelectedFile selectedFile = new SelectedFile() { Id = id, ModifiedDate = date, Title = oldTitle };
+            SelectedFile selectedFile = new SelectedFile() { ModifiedDate = date, Title = oldTitle };
+
+            SelectedFileService selectedFileService = new SelectedFileService(new MainDirectoryService());
+            selectedFileService.Create(selectedFile);
+
+            // Act
+            var result = selectedFileService.UpdateTitle(selectedFile.Id + 1, newTitle);
+
+            // Assert
+            result.Should().Be(-1);
+            selectedFile.ModifiedDate.Should().Be(date);
+            selectedFile.Title.Should().Be(oldTitle);
+        }
+
+        [Fact]
+        public void UpdateTitle_ValidId_ChangesTitleToGivenModifiedDateToNowAndReturnsId()
+        {
+            // Arrange
+            var date = DateTime.Now;
+            string oldTitle = "Old title", newTitle = "New title";
+            SelectedFile selectedFile = new SelectedFile() { ModifiedDate = date, Title = oldTitle };
 
             SelectedFileService selectedFileService = new SelectedFileService(new MainDirectoryService());
             selectedFileService.Create(selectedFile);
