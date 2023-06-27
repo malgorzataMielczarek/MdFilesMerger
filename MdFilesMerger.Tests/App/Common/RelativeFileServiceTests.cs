04232e1aaa2d20e472f67b8c70a2132011cba37b
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-06-27 10:31:53 +0200

    RelativeFileServiceTests - adjust names and simplify tests

--- MdFilesMerger.Tests/App/Common/RelativeFileServiceTests.cs
+++ MdFilesMerger.Tests/App/Common/RelativeFileServiceTests.cs
@@ -8,55 +8,20 @@ using MdFilesMerger.Domain.Entity;
 using Moq;
 using Xunit;
 
-namespace MdFilesMerger.Tests.App.Concrete
+namespace MdFilesMerger.Tests.App.Common
 {
     public class RelativeFileServiceTests
     {
         [Fact]
-        public void CanFindNewFiles()
+        public void FindNewFiles_AllFilesAssociatedWithCollectiond_ReturnsEmpty()
         {
             // Arrange
             var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
             IMainDirectory mainDirectory = new MainDirectory(1, Environment.CurrentDirectory, 1);
-            var files = new FileInfo[15];
-            var newFiles = new FileInfo[5];
-            for (int i = 0; i < 10; i++)
-            {
-                FileInfo file = new FileInfo("file_" + i + ".md");
-                service.Create(new RelativeFileMock(file, mainDirectory));
-                files[i] = file;
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                FileInfo newFile = new FileInfo("newFile_" + i + ".md");
-                newFiles[i] = newFile;
-                files[i + 10] = newFile;
-            }
-
-            // Act
-            var result = service.FindNewFiles(files, mainDirectory);
-
-            //Assert
-            result.Should().BeOfType<List<FileInfo>>();
-            result.Should().NotBeNullOrEmpty();
-            result.Should().HaveCount(5);
-            result.Should().Contain(newFiles);
-        }
-
-        [Fact]
-        public void IsEmptyIfNoNewFiles()
-        {
-            // Arrange
-            var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
-            IMainDirectory mainDirectory = new MainDirectory(1, Environment.CurrentDirectory, 1);
-            var files = new FileInfo[10];
-            for (int i = 0; i < 10; i++)
-            {
-                FileInfo file = new FileInfo("file_" + i + ".md");
-                service.Create(new RelativeFileMock(file, mainDirectory));
-                files[i] = file;
-            }
+            var files = new FileInfo[1];
+            FileInfo file = new FileInfo("file_1.md");
+            service.Create(new RelativeFileMock(file, mainDirectory));
+            files[0] = file;
 
             // Act
             var result = service.FindNewFiles(files, mainDirectory);
@@ -68,27 +33,11 @@ namespace MdFilesMerger.Tests.App.Concrete
         }
 
         [Fact]
-        public void IsEmptyIfEmpty()
+        public void FindNewFiles_AnyNull_ReturnsEmpty()
         {
             // Arrange
             var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
-            IMainDirectory mainDirectory = new MainDirectory(1, Environment.CurrentDirectory, 1);
-
-            // Act
-            var result = service.FindNewFiles(new List<FileInfo>(), mainDirectory);
-
-            //Assert
-            result.Should().BeOfType<List<FileInfo>>();
-            result.Should().NotBeNull();
-            result.Should().BeEmpty();
-        }
-
-        [Fact]
-        public void IsEmptyIfNull()
-        {
-            // Arrange
-            var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
-            IMainDirectory mainDirectory = new MainDirectory(1, Environment.CurrentDirectory, 1);
+            IMainDirectory mainDirectory = new MainDirectory();
 
             // Act
             var result1 = service.FindNewFiles(null, mainDirectory);
@@ -108,7 +57,95 @@ namespace MdFilesMerger.Tests.App.Concrete
         }
 
         [Fact]
-        public void IsReturningItemsOnlyWithGivenMainDirId()
+        public void FindNewFiles_EmptyList_ReturnsEmpty()
+        {
+            // Arrange
+            var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
+            IMainDirectory mainDirectory = new MainDirectory(1, Environment.CurrentDirectory, 1);
+
+            // Act
+            var result = service.FindNewFiles(new List<FileInfo>(), mainDirectory);
+
+            //Assert
+            result.Should().BeOfType<List<FileInfo>>();
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void FindNewFiles_ReturnsFilesNotAssociatedWithCollection()
+        {
+            // Arrange
+            var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
+            IMainDirectory mainDirectory = new MainDirectory(1, Environment.CurrentDirectory, 1);
+            var files = new FileInfo[2];
+
+            FileInfo file = new FileInfo("file.md");
+            service.Create(new RelativeFileMock(file, mainDirectory));
+            files[0] = file;
+
+            FileInfo newFile = new FileInfo("newFile.md");
+            files[1] = newFile;
+
+            // Act
+            var result = service.FindNewFiles(files, mainDirectory);
+
+            //Assert
+            result.Should().BeOfType<List<FileInfo>>();
+            result.Should().NotBeNullOrEmpty();
+            result.Should().HaveCount(1);
+            result.Should().Contain(newFile);
+        }
+
+        [Fact]
+        public void ReadByMainDirId_InvalidMainDirId_ReturnsEmpty()
+        {
+            // Arrange
+            var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
+            service.Create(new RelativeFileMock() { Name = "example.md", MainDirId = 1 });
+
+            // Act
+            var result = service.ReadByMainDirId(2);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ReadByMainDirId_UnsortedCollection_ReturnsSorted()
+        {
+            // Arrange
+            var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
+            RelativeFileMock[] sortedCollection = new RelativeFileMock[10];
+            sortedCollection[0] = new RelativeFileMock() { Name = "Action.md", MainDirId = 0 };
+            sortedCollection[1] = new RelativeFileMock() { Name = "Ex.md", MainDirId = 0 };
+            sortedCollection[2] = new RelativeFileMock() { Name = "Ex0.md", MainDirId = 0 };
+            sortedCollection[3] = new RelativeFileMock() { Name = "Ex3.md", MainDirId = 0 };
+            sortedCollection[4] = new RelativeFileMock() { Name = "Ex11.md", MainDirId = 0 };
+            sortedCollection[5] = new RelativeFileMock() { Name = "Example.md", MainDirId = 0 };
+            sortedCollection[6] = new RelativeFileMock() { Name = "Example1.md", MainDirId = 0 };
+            sortedCollection[7] = new RelativeFileMock() { Name = "Example2.md", MainDirId = 0 };
+            sortedCollection[8] = new RelativeFileMock() { Name = "Example11.md", MainDirId = 0 };
+            sortedCollection[9] = new RelativeFileMock() { Name = "Exemple.md", MainDirId = 0 };
+
+            // Mix up the collection
+            int[] indexes = new int[10] { 4, 0, 8, 1, 3, 5, 9, 7, 2, 6 };
+            foreach (int i in indexes)
+            {
+                service.Create(sortedCollection[i]);
+            }
+
+            // Act
+            var result = service.ReadByMainDirId(0);
+
+            // Assert
+            result.Should().ContainInOrder(sortedCollection);
+            result.Should().BeInAscendingOrder();
+        }
+
+        [Fact]
+        public void ReadByMainDirId_ValidMainDirId_ReturnsOnlyItemsWithGivenMainDirId()
         {
             // Arrange
             var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
@@ -144,81 +181,33 @@ namespace MdFilesMerger.Tests.App.Concrete
         }
 
         [Fact]
-        public void IsReturningSorted()
-        {
-            // Arrange
-            var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
-            RelativeFileMock[] files = new RelativeFileMock[10];
-            files[0] = new RelativeFileMock() { Name = "Action.md", MainDirId = 0 };
-            files[1] = new RelativeFileMock() { Name = "Ex.md", MainDirId = 0 };
-            files[2] = new RelativeFileMock() { Name = "Ex0.md", MainDirId = 0 };
-            files[3] = new RelativeFileMock() { Name = "Ex3.md", MainDirId = 0 };
-            files[4] = new RelativeFileMock() { Name = "Ex11.md", MainDirId = 0 };
-            files[5] = new RelativeFileMock() { Name = "Example.md", MainDirId = 0 };
-            files[6] = new RelativeFileMock() { Name = "Example1.md", MainDirId = 0 };
-            files[7] = new RelativeFileMock() { Name = "Example2.md", MainDirId = 0 };
-            files[8] = new RelativeFileMock() { Name = "Example11.md", MainDirId = 0 };
-            files[9] = new RelativeFileMock() { Name = "Exemple.md", MainDirId = 0 };
-
-            int[] indexes = new int[10] { 4, 0, 8, 1, 3, 5, 9, 7, 2, 6 };
-            foreach (int i in indexes)
-            {
-                service.Create(files[i]);
-            }
-
-            // Act
-            var result = service.ReadByMainDirId(0);
-
-            // Assert
-            result.Should().ContainInOrder(files);
-            result.Should().BeInAscendingOrder();
-        }
-
-        [Fact]
-        public void IsReturningEmptyForInvalidMainDirId()
-        {
-            // Arrange
-            var service = new RelativeFileService<RelativeFileMock>(new MainDirectoryService());
-            for (int i = 1; i < 10; i++)
-            {
-                service.Create(new RelativeFileMock() { Name = i.ToString(), MainDirId = i / 2 });
-            }
-
-            // Act
-            var result = service.ReadByMainDirId(-6);
-
-            // Assert
-            result.Should().NotBeNull();
-            result.Should().BeEmpty();
-        }
-
-        [Fact]
-        public void CanUpdatePath()
+        public void UpdatePath_InvalidId_RenturnsMinusOne()
         {
             // Arrange
             IMainDirectory mainDirectory = new MainDirectory(1, Environment.CurrentDirectory, 1);
             var mock = new Mock<IMainDirectoryService>();
-            mock.Setup(s => s.ReadById(1)).Returns(mainDirectory);
+            mock.Setup(s => s.ReadById(It.IsAny<int>()));
+
             var service = new RelativeFileService<RelativeFileMock>(mock.Object);
-            string oldPath = "oldPath.md", newPath = Path.Combine("path", "newPath.md");
-            var file = new RelativeFileMock(new FileInfo(oldPath), mainDirectory);
+            var file = new RelativeFileMock();
             service.Create(file);
 
             // Act
-            var result = service.UpdatePath(file.Id, Path.GetFullPath(newPath));
+            var result = service.UpdatePath(file.Id + 1, "newPath.md");
 
             // Assert
-            result.Should().Be(file.Id);
-            file.Name.Should().NotBe(oldPath).And.Be(newPath);
+            result.Should().Be(-1);
+            mock.Verify(s => s.ReadById(It.IsAny<int>()), Times.Never());
         }
 
         [Fact]
-        public void IsNotUpdatingPathIfNull()
+        public void UpdatePath_ValidIdAndNull_KeepsOldPathAndReturnsMinusOne()
         {
             // Arrange
             IMainDirectory mainDirectory = new MainDirectory(1, Environment.CurrentDirectory, 1);
             var mock = new Mock<IMainDirectoryService>();
             mock.Setup(s => s.ReadById(1)).Returns(mainDirectory);
+
             var service = new RelativeFileService<RelativeFileMock>(mock.Object);
             string oldPath = "oldPath.md";
             var file = new RelativeFileMock(new FileInfo(oldPath), mainDirectory);
@@ -233,23 +222,24 @@ namespace MdFilesMerger.Tests.App.Concrete
         }
 
         [Fact]
-        public void IsNotUpdatingPathIfInvalidId()
+        public void UpdatePath_ValidIdAndPath_SetsNameToNewPathAndReturnsId()
         {
             // Arrange
             IMainDirectory mainDirectory = new MainDirectory(1, Environment.CurrentDirectory, 1);
             var mock = new Mock<IMainDirectoryService>();
             mock.Setup(s => s.ReadById(1)).Returns(mainDirectory);
+
             var service = new RelativeFileService<RelativeFileMock>(mock.Object);
             string oldPath = "oldPath.md", newPath = Path.Combine("path", "newPath.md");
             var file = new RelativeFileMock(new FileInfo(oldPath), mainDirectory);
             service.Create(file);
 
             // Act
-            var result = service.UpdatePath(file.Id + 1, Path.GetFullPath(newPath));
+            var result = service.UpdatePath(file.Id, Path.GetFullPath(newPath));
 
             // Assert
-            result.Should().Be(-1);
-            file.Name.Should().Be(oldPath);
+            result.Should().Be(file.Id);
+            file.Name.Should().NotBe(oldPath).And.Be(newPath);
         }
 
         private class RelativeFileMock : RelativeFile
