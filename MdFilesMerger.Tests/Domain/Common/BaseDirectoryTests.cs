33f6651d2949c352dc77a3fc3222d07d4e002244
mielczarek <malgorzata.mielczarek@unityline.pl>
2023-06-29 10:28:26 +0200

    BaseDirectoryTests - adjust names and simplify tests

--- MdFilesMerger.Tests/Domain/Common/BaseDirectoryTests.cs
+++ MdFilesMerger.Tests/Domain/Common/BaseDirectoryTests.cs
@@ -1,6 +1,5 @@
 ﻿using FluentAssertions;
 using MdFilesMerger.Domain.Common;
-using MdFilesMerger.Domain.Entity;
 using Xunit;
 
 namespace MdFilesMerger.Tests.Domain.Common
@@ -8,91 +7,118 @@ namespace MdFilesMerger.Tests.Domain.Common
     public class BaseDirectoryTests
     {
         [Fact]
-        public void IsComparingCorrectly()
+        public void CompareTo_EquivalentIDirectory_ReturnsZero()
         {
             // Arrange
-            BaseDirectory dir10a = new BaseDirectory() { Name = "dir10/dir10a" };
-            BaseDirectory dir10 = new BaseDirectory() { Name = "dir10" };
-            BaseDirectory dir = new BaseDirectory() { Name = "dir" };
-            BaseDirectory dir2 = new BaseDirectory() { Name = "dir2" };
-            BaseDirectory dirBis = new BaseDirectory() { Name = "dir" };
-            BaseDirectory file10 = new BaseDirectory() { Name = "dir10/file10.md" };
-            BaseDirectory file10a = new BaseDirectory() { Name = "dir10/dir10a/file10.md" };
+            BaseDirectory thisDir = new BaseDirectory() { Name = "equal" };
+            BaseDirectory otherDir = new BaseDirectory() { Name = "equal" };
 
             // Act
-            var equal = new int[] { dir.CompareTo(dirBis), dirBis.CompareTo(dir) };
-            var first = new int[] { dir.CompareTo(dir10), dir.CompareTo(dir2), dir10.CompareTo(dir10a), dir10.CompareTo(file10), dir10a.CompareTo(file10), file10.CompareTo(file10a), dir2.CompareTo(dir10) };
-            var second = new int[] { dir10.CompareTo(dir), dir2.CompareTo(dir), dir10a.CompareTo(dir10), file10.CompareTo(dir10), file10.CompareTo(dir10a), file10a.CompareTo(file10), dir10.CompareTo(dir2) };
+            var result = thisDir.CompareTo(otherDir);
 
             // Assert
-            equal.Should().AllBeEquivalentTo(0);
-            first.Should().AllSatisfy(x => x.Should().BeLessThan(0));
-            second.Should().AllSatisfy(x => x.Should().BeGreaterThan(0));
+            result.Should().Be(0);
         }
 
-        [Fact]
-        public void CanSetRootedPath()
+        [Theory]
+        [InlineData("dir", "dir2")]
+        [InlineData("dir2", "dir10")]
+        [InlineData("dir10", "dir10/dir10a")]
+        [InlineData("dir10/dir10a", "dir10/file10.md")]
+        [InlineData("dir10/file10.md", "dir10/dir10a/file10.md")]
+        public void CompareTo_FollowingIDirectory_ReturnsLessThanZero(string thisName, string otherName)
         {
             // Arrange
-            string oldPath = "this/is/old/path", newPath = "C:/this/is/new/path";
-            var newPathsFormats = new string[] { newPath, "C:\\this\\is\\new\\path" };
-            BaseDirectory baseDirectory = new BaseDirectory(1, oldPath, DateTime.Now);
+            BaseDirectory thisDir = new BaseDirectory() { Name = thisName };
+            BaseDirectory otherDir = new BaseDirectory() { Name = otherName };
 
-            foreach (var format in newPathsFormats)
-            {
-                DateTime oldModDat = baseDirectory.ModifiedDate;
+            // Act
+            var result = thisDir.CompareTo(otherDir);
 
-                // Act
-                var result = baseDirectory.SetPath(format);
-
-                // Assert
-                result.Should().BeTrue();
-                baseDirectory.Name.Should().Be(newPath);
-                baseDirectory.ModifiedDate.Should().BeAfter(oldModDat).And.BeBefore(DateTime.Now);
-            }
+            // Assert
+            result.Should().BeLessThan(0);
         }
 
-        [Fact]
-        public void CanSetNotRootedPath()
+        [Theory]
+        [InlineData("dir2", "dir")]
+        [InlineData("dir10", "dir2")]
+        [InlineData("dir10/dir10a", "dir10")]
+        [InlineData("dir10/file10.md", "dir10/dir10a")]
+        [InlineData("dir10/dir10a/file10.md", "dir10/file10.md")]
+        public void CompareTo_PrecedingIDirectory_ReturnsGreaterThanZero(string thisName, string otherName)
         {
             // Arrange
-            string oldPath = "this/is/old/path", newPath = "this/is/new/path";
-            var newPathsFormats = new string[] { newPath, "this\\is\\new\\path" };
-            BaseDirectory baseDirectory = new BaseDirectory(1, oldPath, DateTime.Now);
+            BaseDirectory thisDir = new BaseDirectory() { Name = thisName };
+            BaseDirectory otherDir = new BaseDirectory() { Name = otherName };
 
-            foreach (var format in newPathsFormats)
-            {
-                DateTime oldModDat = baseDirectory.ModifiedDate;
+            // Act
+            var result = thisDir.CompareTo(otherDir);
 
-                // Act
-                var result = baseDirectory.SetPath(format);
-
-                // Assert
-                result.Should().BeTrue();
-                baseDirectory.Name.Should().Be(newPath);
-                baseDirectory.ModifiedDate.Should().BeAfter(oldModDat).And.BeBefore(DateTime.Now);
-            }
+            // Assert
+            result.Should().BeGreaterThan(0);
         }
 
-        [Fact]
-        public void IsPathNullIfInvalid()
+        [Theory]
+        [InlineData("        ")]
+        [InlineData("th*s/*s/inval*d/path")]
+        [InlineData("another>path")]
+        [InlineData("file?")]
+        [InlineData("other|file")]
+        [InlineData("one\tmore\\path")]
+        [InlineData("not\\in:\\root")]
+        [InlineData(null)]
+        public void SetPath_InvalidPath_SetsNameToNullAndReturnsFalse(string? inputPath)
         {
             // Arrange
             string oldPath = "this/is/old/path";
             BaseDirectory baseDirectory = new BaseDirectory(1, oldPath, DateTime.Now);
+            DateTime oldModDat = baseDirectory.ModifiedDate;
 
-            foreach (var newPath in new string[] { "        ", "th*s/*s/inval*d/path", "another>path", "file?", "other|file", "one\tmore\\path", "not\\in:\\root", null })
-            {
-                DateTime oldModDat = baseDirectory.ModifiedDate;
+            // Act
+            var result = baseDirectory.SetPath(inputPath);
 
-                // Act
-                var result = baseDirectory.SetPath(newPath);
+            // Assert
+            result.Should().BeFalse();
+            baseDirectory.Name.Should().BeNull();
+            baseDirectory.ModifiedDate.Should().Be(oldModDat);
+        }
 
-                // Assert
-                result.Should().BeFalse();
-                baseDirectory.Name.Should().BeNull();
-                baseDirectory.ModifiedDate.Should().Be(oldModDat);
-            }
+        [Theory]
+        [InlineData("this/is/new/path", "this/is/new/path")]
+        [InlineData("this\\is\\new\\path", "this/is/new/path")]
+        public void SetPath_NotRootedPath_ChangesNameToFormatedGivenPathModifiedDateToNowAndReturnsTrue(string inputPath, string formatedPath)
+        {
+            // Arrange
+            string oldPath = "this/is/old/path";
+            BaseDirectory baseDirectory = new BaseDirectory(1, oldPath, DateTime.Now);
+            DateTime oldModDat = baseDirectory.ModifiedDate;
+
+            // Act
+            var result = baseDirectory.SetPath(inputPath);
+
+            // Assert
+            result.Should().BeTrue();
+            baseDirectory.Name.Should().Be(formatedPath);
+            baseDirectory.ModifiedDate.Should().BeAfter(oldModDat).And.BeBefore(DateTime.Now);
+        }
+
+        [Theory]
+        [InlineData("C:/this/is/new/path", "C:/this/is/new/path")]
+        [InlineData("C:\\this\\is\\new\\path", "C:/this/is/new/path")]
+        public void SetPath_RootedPath_ChangesNameToFormatedGivenPathModifiedDateToNowAndReturnsTrue(string inputPath, string formatedPath)
+        {
+            // Arrange
+            string oldPath = "this/is/old/path";
+            BaseDirectory baseDirectory = new BaseDirectory(1, oldPath, DateTime.Now);
+            DateTime oldModDat = baseDirectory.ModifiedDate;
+
+            // Act
+            var result = baseDirectory.SetPath(inputPath);
+
+            // Assert
+            result.Should().BeTrue();
+            baseDirectory.Name.Should().Be(formatedPath);
+            baseDirectory.ModifiedDate.Should().BeAfter(oldModDat).And.BeBefore(DateTime.Now);
         }
     }
 }
